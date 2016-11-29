5ce8b5e89ae5d5690fe283c8c7deef6ec1cdd9c1
Pat <charland.patrick@carrefour.cegepvicto.ca>
2016-11-29 11:59:24 -0500

    Added ExamenFormatif3 project
    Added project.
    Added question 3 answer
    Added question 5 answer

---
+++ ExamenFormatif3/ExamenFormatif3_Question3/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+ * Auteur: Patrick Charland
+ * 29-11-2016
+ * Description: Examen formatif 3
+ */
+
+namespace ExamenFormatif3_Question3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Déclaration variables
+            int nbChoixJoueur = 0;
+            int nbSomme = 0;
+            bool pair = true;
+
+            //Prise de données
+            Console.WriteLine("Veuillez choisir un nombre: ");
+            Console.Write("Choix: ");
+            nbChoixJoueur = int.Parse(Console.ReadLine());
+
+            //Calcul
+            if((nbChoixJoueur%2)==0)
+            {
+                pair = true;
+            }
+            else
+            {
+                pair = false;
+            }
+
+            for (int i = 0; i <= nbChoixJoueur;)
+            {
+                if(pair)
+                {
+                    nbSomme += i;
+                    i += 2;
+                    Console.Write(nbSomme + " ");
+                }
+                else
+                {
+                    nbSomme += i;
+                    if (i==0)
+                    {
+                        i += 1;
+                        
+                    }
+                    else
+                    {
+                        i += 2;
+                    }      
+                    Console.Write(nbSomme + " ");
+                }
+            }
+            Console.WriteLine("\n"+nbSomme);
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ ExamenFormatif3/ExamenFormatif_Question5/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/*
+ * Auteur: Patrick Charland
+ * 29-11-2016
+ * Description: Exercice sur For Loop.
+ */
+namespace ExamenFormatif_Question5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Variables
+            bool[] tableGameBoard = new bool[100];
+            int nbRandomValue = 0;
+            Random rnd = new Random();
+
+            //Génération du tableau
+            for(int i = 0; i<tableGameBoard.Length;i++)
+            {
+                nbRandomValue = rnd.Next(2);
+                if (nbRandomValue == 0)
+                {
+                    tableGameBoard[i] = true;
+                }
+                else
+                {
+                    tableGameBoard[i] = false;
+                }
+                Console.Write(tableGameBoard[i]+" ");
+            }
+
+        }
+    }
+}
